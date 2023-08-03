713e9f480c0fec87b1d809beaf7e2c69f206664c
Dmitriy Palii <it.n00b.pel@gmail.com>
2023-08-03 13:38:34 +0300

    add Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,25 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run(): void {
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::factory(21)->create();
+        $tags = Tag::factory(37)->create();
+        $posts = Post::factory(98)->create();
+
+        $posts->map(function ($post) use ($tags) {
+            $tagsCount = random_int(1, 9);
+            $tagsForPostIds = $tags->random($tagsCount)->pluck('id');
+            $post->tags()->attach($tagsForPostIds);
+        });
     }
 }
