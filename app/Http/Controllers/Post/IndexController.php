3a102d4834cd720665e848dd974cf08a7405aa4d
Dmitriy Palii <it.n00b.pel@gmail.com>
2023-08-04 13:09:44 +0300

    add filter

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\PostFilter;
+use App\Http\Requests\Post\FilterRequest;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\Tag;
@@ -12,11 +14,15 @@ use Illuminate\Pagination\Paginator;
 
 class IndexController extends BaseController {
     //
-    public function __invoke() {
+    public function __invoke(FilterRequest $request) {
 
-        $posts = Post::paginate(10);
-//        dd($posts);
-        return view('post.index', compact('posts'));
+//        $posts = Post::paginate(10);
+        $data = $request->validated();
+        $filter = app()->make(PostFilter::class, ['queryParams' => array_filter($data)]);
+        $posts = Post::filter($filter)->paginate(10);
+            return $posts;
+//       dd($posts);
+//        return view('post.index', compact('posts'));
     }
 
 }
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -2,12 +2,12 @@
 @section('content')
     <div class="w-75 m-auto">
 
-    @foreach($posts as $post)
-      <div><a href="{{route('post.show', $post->id)}}">{{$post->title}}</a></div>
-    @endforeach
+        @foreach($posts as $post)
+            <div><a href="{{route('post.show', $post->id)}}">{{$post->title}}</a></div>
+        @endforeach
         <a href="{{route('post.create')}}">
             <button type="button" class="btn btn-dark">Add one</button>
         </a>
-        {{$posts->links()}}
+        {{$posts->withQueryString() ->links()}}
     </div>
 @endsection
